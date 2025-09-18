859c656da3b43a5cd31f130ab9c25ca117fd3855
Sumon <msk.sumon.kaysar@gmail.com>
2025-09-18 15:12:03 +0600

    added index router in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import router from "./app/routes";
 
 const app: Application = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1", router);
 
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({
