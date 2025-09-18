91d2b2397994c184660f030bfa19a155b293c856
simanta <simanta.barua@goama.com>
2025-09-19 00:14:28 +0600

    feat(structure): create initial backend folder structure with all modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,23 @@
-// @ts-check
-
-import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import tseslint from "typescript-eslint";
-
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  {
-    rules: {
-      "no-unused-vars": [
-        "warn",
-        {
-          varsIgnorePattern: "^_",
-        },
-      ],
-      "no-unused-expressions": "error",
-      "prefer-const": "error",
-      "no-console": "warn",
-    },
-  }
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    rules: {
+      "no-unused-vars": [
+        "warn",
+        {
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+    },
+  }
 );
